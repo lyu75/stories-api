7a4d36f88e3f0bd358bbf5b0ef679062f8301449
Nox Yu <nox19990209@gmail.com>
2019-07-27 10:25:41 +0800

    setup db

--- db/migrate/20190727021449_create_comments.rb
+++ db/migrate/20190727021449_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :name
       t.text :content
-      t.reference :story
+      t.references :story
 
       t.timestamps
     end
